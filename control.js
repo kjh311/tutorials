0eea2306a40d6bb9db966eba861949f3efc478ce
kjh311 <kjh311@gmail.com>
2017-10-27 15:14:38 -0700

    asdf

--- control.js
+++ control.js
@@ -10,58 +10,58 @@ app.controller('myCtrl', ['$scope', function($scope) {
    $scope.links = [
        {
             "name" : "A/B Testing",
-            "link" : "AB_testing.html"
+            "link" : "https://docs.google.com/document/d/1oAGs_4qRMF_1G-TeuS1CG8ClVcTWxuLYLDcal7l5DHU/edit"
         },{
             "name" : "Angular",
-            "link" : "angular.html"
+            "link" : "https://docs.google.com/document/d/1CTJMwlcXYNZMBGXT8JgOTQ9-tVLU-YyOz0k_eRq4Pps/edit"
         },{
             "name" : "Hide API Keys w/Node.js",
-            "link" : "hide_api_key_node.html"
+            "link" : "https://docs.google.com/document/d/15wFpFjs7LvGTzs1ekeDXch9xHN3lMqisNyZ1H1hrZi8/edit"
         },{
             "name" : "Javascript",
-            "link" : "javascript.html"
+            "link" : "https://docs.google.com/document/d/171EGFnSvRig9RXdj-NBaVlqW_i4ZqnzuOmau7k1-jL8/edit?usp=docs_web"
         },{
             "name" : "Setting up Node",
-            "link" : "node.html"
+            "link" : "https://docs.google.com/document/d/1dcVEbqgiQWRsKoSmqhDew4PHXXY-lWnxQzT_IyKWtpo/edit?usp=docs_web"
         },{
             "name" : "Host on Github",
-            "link" : "host_on_github.html"
+            "link" : "https://docs.google.com/document/d/1Kp40EZMo_eTNR5jGpYLfkPmUMbqCABaJktazV281sgs/edit"
         },{
             "name" : "PHP",
-            "link" : "PHP.html"
+            "link" : "https://docs.google.com/document/d/1GH7H-oEaDK6bVsSHVAVrG6iF_6eSrR30Yyh556fe3nc/edit?usp=docs_web"
         },{
             "name" : "Test Driven Development",
-            "link" : "TDD.html"
+            "link" : "https://docs.google.com/document/d/109SwWYqAeFWiugb70tGhb55CBvdfg6Zl0XJD7iDWqyE/edit"
         },{
             "name" : "AJAX",
-            "link" : "ajax.html"
+            "link" : "https://docs.google.com/document/d/1tuIvVJCGadV2EwZVG3la5yHq4VaGkX9ehOD0KzXd8ls/edit#"  
         },{
             "name" : "Animation JS and CSS",
-            "link" : "Animation.html"
+            "link" : "https://docs.google.com/document/d/1q8YlhZ3wLaIdHq0Ew-HG60cN0LaEPYEKmXABbV1D5_E/edit"
         },{
             "name" : "CDNs",
-            "link" : "CDN.html"
+            "link" : "https://docs.google.com/document/d/17howIS3Vj_Fy0vM1hPUwBVl607KdKElQRcclNlm-1iA/edit"
         },{
             "name" : "REACT",
-            "link" : "react.html"
+            "link" : "https://docs.google.com/document/d/1I82GYM1u3-im7mFgE7BtMD0lEAKE8elGdbxA-gEd2E4/edit?usp=docs_web"
         },{
             "name" : "DESIGN",
-            "link" : "design.html"
+            "link" : "https://docs.google.com/document/d/1oyS5L3g1k6fc1YqO6ZO68bH-i_ZonedRgDhFXdfj3h8/edit"
         },{
             "name" : "GIT",
-            "link" : "git.html"
+            "link" : "https://docs.google.com/document/d/15i7lnZ1B-XPd3oZE0fbO9sX_WNk7EV9ug4S0CZYxxsE/edit#"
         },{
             "name" : "Wordpress Theme Development",
-            "link" : "wordpress.html"
+            "link" : "https://docs.google.com/document/d/1fRdGC3BM9gyCqQ3ahjTbsyKcZEdqTWSkDLjzeDv-JyM/edit"
         },{
             "name" : "MongoDB",
-            "link" : "MongoDB.html"
+            "link" : "https://docs.google.com/document/d/1VPJaVD1U6Pys10iJ0YlqKwUeje9UqNPaS20vDULNeog/edit#"
         },{
            "name" : "Backing up Macbook After a crash",
-            "link" : "backing-up-macbook.html"
+            "link" : "https://docs.google.com/document/d/1OenAKONyRY-lHPlMExtdiSEg_2nROxwiMxUHCEwl9o8/edit#heading=h.a91sogvsdzai"
         },{
            "name" : "Machine Learning",
-            "link" : "machineLearning.html"
+            "link" : "https://docs.google.com/document/d/1wcF7KJYxD6WGewsQrTCUBaoL80_JaIyuC6QNmzre9eM/edit"
         }
     ]
 
